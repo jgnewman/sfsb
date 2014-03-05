c0965fedaf5802dc8ecce62d9a96431a4b375fb1
John Newman <rescuecreative@gmail.com>
2014-03-05 17:06:20 -0500

    add ajax polling and some todos

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 Solid Fuel Socket Booster
 ===
 
-> A combination WebSocket and WebWorker library.
+> A combination WebSocket, Ajax, and WebWorker library.
 
-SFSB supercharges a WebSocket by running it within a web worker and allowing
-you to process the data it hands you within that worker before sending the data
-back to the main thread.
+SFSB supercharges a WebSockets and ajax server polling by running your code
+within a web worker and allowing you to process the data it hands you within
+that worker before sending the data back to the main thread.
+
+Compatibility
+-------------
+
+SFSB is compatible with IE10+ and all recent versions of Firefox and Chrome.
+Further backward compatibility with IE would be self-defeating in that you
+would have to fake the web worker API, run that code in your main application
+thread, and thus defeat the whole point of the library.
 
 Setup
 -----
 
-The library is built using Gulp and Browserify and is therefore available
-using Node and the browser. Just plug it in and you're good to go.
+The library is built using Gulp and Browserify. Just plug it in and you're
+good to go.
 
 ### For Development
 
@@ -28,26 +36,35 @@ API
 
 SFSB is extremely convenient in that it does not require you to write any new
 JavaScript files in order to spawn your web worker. It takes care of all that
-for you using the magic of the `Blob` and `createObjectURL`.
+for you using the magic of the `Blob` and `createObjectURL`. Essentially, you'll
+never have to worry about interfacing with the web worker at all. Here's what
+you do instead:
 
-The idea is that you will create a `new SFSB` and hand it two things: a
-websocket url and, optionally, a function that will be used to process data
-received from the websocket:
+### WebSockets
+
+The idea here is that you will create a `new SF.socketBooster` and hand it two
+things: a websocket url and, optionally, a function that will be used to process
+data received from the websocket:
 
 ```javascript
-var socket = new SFSB('ws://echo.websocket.org', function (data) {
+var socket = new SF.socketBooster('ws://echo.websocket.org', function (data) {
   return data + '!!!';
 });
 ```
 
 In this example, we set up a websocket connection to an echo server. Whatever
-we send to this server will be echoed back. We also pass a function that will
-be executed within the web worker instead of within the main application thread.
+we send to this server will be echoed back. Don't forget, this socket is
+running inside a web worker. We also passed in a function that will be executed
+within that web worker instead of within the main application thread.
 This function takes the data given to us by the socket and adds "!!!" to the
 end of it. Having been processed, this data will now be sent back to our
 main thread.
 
-In order to get the data back out of our socket, we'll add an event listener
+> **Note:** Because your processor function runs inside the web worker, it will
+> not have access to any closure data from your main thread. It must be
+> entirely independent of any outside state.
+
+In order to get data back out of our socket, we'll add an event listener
 to it:
 
 ```javascript
@@ -57,7 +74,7 @@ socket.addEventListener('message', function (evt) {
 ```
 
 The above call registers a function to be run whenever we receive a message
-back from our socket/worker.
+back from our socket worker.
 
 At this point we can test whether or not everything is working by sending a
 message to the socket. Even though the socket is running within a web worker,
@@ -75,5 +92,112 @@ runs our data processor function which adds "!!!" to the end. Once this is
 finished, the data is finally handed back to the main thread and picked up by
 our event listener which logs to the console: `The message was hello!!!`.
 
-More coming soon...
+### Ajax
+
+In the case of ajax server polling, the whole point is that you don't have
+websockets available and you therefore need to continually make requests to the
+server to retrieve potentially new data. This, of course, can become taxing on
+your main thread so SFSB gives you a way to set up a convenient ajax poller in
+a web worker.
+
+Begin by creating a `new SF.ajaxPoller` and handing it a somewhat
+familiar-looking ajax settings object:
+
+```javascript
+var ajax = new SF.ajaxPoller({
+  url: 'http://www.example.com',
+  timeout: 10000,
+  frequency: 30000,
+  headers: {},
+  data: {},
+  process: function (data) {}
+});
+```
+
+In this example we can see all of our available options. Because you are polling
+the server for updates, there is no need to specify the request method. It will
+be "GET" by default. Following are the meanings of each setting. Note that
+only `url` is required. All others are optional.
+
+- `url` - The url of the server where requests will be made.
+- `timeout` - How long any given request should wait before timing out. The
+default is 10000 (or 10 seconds).
+- `frequency` - How often to poll the server. The default is 30000 (or every
+30 seconds).
+- `headers` - Where each key is a header name and each value is a header value.
+This is where you can add CSRF tokens, for example.
+- `data` - Any data that should be sent to the server along with the request.
+- `process` - A function that will be used to process data in the event of a
+successful response. Whatever this function returns will be passed back to the
+main application thread.
+
+> **Note:** Because your `process` function runs inside the web worker, it will
+> not have access to any closure data from your main thread. It must be
+> entirely independent of any outside state.
+
+To receive data back from your ajax requests, simply add event listeners to you
+ajax poller:
+
+```javascript
+ajax.addEventListener('success', function (data) {
+  console.log('I received:', data);
+});
+```
+
+You can add event listeners for any of three events: `success`, `error`, and
+`message`.
+
+Success listeners will only run when the ajax request returns a request with
+a status in the 200-299 range.
+
+Error listeners will run when the response gives us a status outside the 200s
+or otherwise produces an error,  when the web worker produces an error, and
+when the request times out.
+
+Message listeners will run whenever the worker hands us anything. This happens
+on both `success` and `error` events.
+
+Sometimes you may find yourself needing to send a PUT/POST/DELETE request to
+the same server you are polling for updates. In that case, you can call any
+of the `put`, `post`, or `del` methods on the ajax poller. For example:
+
+```javascript
+ajax.post({
+  data: {},
+  headers: {},
+  timeout: 10000
+});
+```
+
+Notice that when using any of these methods, you only have the ability to
+specify a timeout, new headers, and data specific to the request. If you choose
+not to specify any given one of these settings, it will use the defaults
+you provided when you initially created your ajax poller.
+
+Note that PUT/POST/DELETE requests happen independently of your GET polling.
+In other words, polling will still continue in the background while your new
+request is open and polling frequency will not be interrupted by a response from
+your new request. For instance, you may be polling a server every 30 seconds
+and then, 15 seconds in, you might make a POST request. Assuming that POST
+returns fairly quickly, your success listeners will pick up on the response
+from that POST and then 15 seconds later, pick up on the response from the
+next poll.
+
+TODO
+----
+
+- Periodically refresh ajax workers to clear out memory
+- Add ways to manually kill sockets and ajax workers
+- Maybe add ways to revive dead sockets and ajax workers
+
+
+
+
+
+
+
+
+
+
+
 
--- dist/sfsb.js
+++ dist/sfsb.js
@@ -1,8 +1,296 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// shim for using process in browser
+
+var process = module.exports = {};
+
+process.nextTick = (function () {
+    var canSetImmediate = typeof window !== 'undefined'
+    && window.setImmediate;
+    var canPost = typeof window !== 'undefined'
+    && window.postMessage && window.addEventListener
+    ;
+
+    if (canSetImmediate) {
+        return function (f) { return window.setImmediate(f) };
+    }
+
+    if (canPost) {
+        var queue = [];
+        window.addEventListener('message', function (ev) {
+            var source = ev.source;
+            if ((source === window || source === null) && ev.data === 'process-tick') {
+                ev.stopPropagation();
+                if (queue.length > 0) {
+                    var fn = queue.shift();
+                    fn();
+                }
+            }
+        }, true);
+
+        return function nextTick(fn) {
+            queue.push(fn);
+            window.postMessage('process-tick', '*');
+        };
+    }
+
+    return function nextTick(fn) {
+        setTimeout(fn, 0);
+    };
+})();
+
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+}
+
+// TODO(shtylman)
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
+
+},{}],2:[function(require,module,exports){
+(function (process){
 
 var global    = window,
     secretary = require('./secretary'),
-    task;
+    wstask,
+    pltask;
+
+/**
+ * Converts data into query string format.
+ * Intended to be stringified and run within a Web Worker.
+ *
+ * @param {Object|String} data - Data to be converted.
+ *
+ * @returns {String} - In the format of a url query string.
+ */
+function stringify(data) {
+  var output = '';
+
+  /*
+   * If the data is already a string, return it.
+   */
+  if (typeof data === 'string') {
+    return data;
+  }
+
+  /*
+   * Iterate over the values and convert them
+   * into the &param=value format.
+   */
+  Object.keys(data).forEach(function (key) {
+    var value = data[key];
+
+    /*
+     * If the value is some kind of object, iterate over it
+     * and appropriately create values.
+     */
+    if (typeof value === 'object') {
+
+      /*
+       * Create array items.
+       */
+      if (Array.isArray(value)) {
+        value.forEach(function (arrItem) {
+          output += ('&' + key + '[]=' + encodeURIComponent(arrItem));
+        });
+
+      /*
+       * Create object values.
+       */
+      } else {
+        Object.keys(value).forEach(function (subKey) {
+          output += ('&' + key + '{' + subKey + '}=' + encodeURIComponent(value[subKey]));
+        });
+      }
+
+    /*
+     * Otherwise, create a value simply.
+     */
+    } else {
+      output += ('&' + key + '=' + encodeURIComponent(value));
+    }
+  });
+
+  /*
+   * Slice the unnecessary '&' off of the beginning.
+   */
+  return output.slice(1);
+}
+
+/**
+ * Creates an ajax request.
+ * Intended to be stringified and run within a Web Worker.
+ *
+ * @param {Object} settings - A settings object for ajax.
+ *
+ * @key {String} url       - The request URL.
+ * @key {String} type      - The REST method. Defaults to 'GET'.
+ * @key {Object} data      - Data to send with the request.
+ * @key {Number} timeout   - How long before the request gives up.
+ * @key {Number} frequency - How often to poll the server.
+ * @key {Object} headers   - Defaults to native defaults. Should
+ *                           be in the format of {contentType: 'text/plain'}
+ *
+ * @returns {undefined}
+ */
+function createXHR(settings) {
+  var req    = new XMLHttpRequest(),
+      method = (settings.type ? settings.type.toUpperCase() : 'GET'),
+      url,
+      data,
+      timer;
+
+  /*
+   * Stringify any data being sent.
+   */
+  Object.prototype.hasOwnProperty.call(settings, "data") &&
+    (data = stringify(settings.data));
+
+  /*
+   * Add in the question mark for GET requests.
+   */
+  method === 'GET' && (data = '?' + data);
+
+  /*
+   * Attach query strings to the URL for a GET request.
+   */
+  url = settings.url + (method === 'GET' ? (data || '') : '');
+
+  /*
+   * Open the request asynchronously using either
+   * the user-provided method or 'GET'.
+   */
+  req.open(method, url, true);
+
+  /*
+   * Prepare the request to send encoded data.
+   * Users will be able to override this.
+   */
+  req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+  /*
+   * Add user-supplied headers.
+   */
+  Object.prototype.hasOwnProperty(settings, "headers") &&
+    Object.keys(settings.headers).forEach(function (key) {
+      req.setRequestHeader(key, settings.headers[key]);
+    });
+
+  /*
+   * Send the request. Include data if appropriate.
+   */
+  req.send(method === 'POST' || method === 'DELETE' || method === 'PUT' ? (data || null) : null);
+
+  /*
+   * Initialize a timer so the request will die if it takes too long.
+   */
+  timer = setTimeout(function () {
+    req.abort();
+    self.postMessage({"result"   : "error",
+                      "response" : "timeout",
+                      "status"   : xhr.status});
+  }, 10000 || settings.timeout || req.timeout);
+
+  /*
+   * Once the request has loaded...
+   */
+  req.addEventListener('load', function (evt) {
+    var status = parseInt(evt.srcElement.status);
+
+    /*
+     * Clear the timer.
+     */
+    clearTimeout(timer);
+
+    /*
+     * If the request status does not fall into the successful range,
+     * reject the promise.
+     */
+    if (status < 200 || status > 299) {
+      self.postMessage({"result"   : "error",
+                        "response" : evt.srcElement.response,
+                        "status"   : evt.srcElement.status});
+
+    /*
+     * In the event of a good result, process that result and hand it back.
+     */
+    } else {
+      self.postMessage({"result"   : "success",
+                        "response" : (process
+                                        ? process(evt.srcElement.response)
+                                        : evt.srcElement.response),
+                        "status"   : evt.srcElement.status});
+
+    }
+  });
+
+  /*
+   * If the request errors out, clear the timer and
+   * reject the promise.
+   */
+  req.addEventListener('error', function (evt) {
+    clearTimeout(timer);
+    self.postMessage({"result"   : "error",
+                      "response" : evt.srcElement.response,
+                      "status"   : evt.srcElement.status});
+  });
+
+  /*
+   * The purpose of this worker is to continue to poll the server
+   * every so often so, if this wasn't a PUT/POST/DELETE, countdown
+   * and then make another request.
+   */
+  return method === "GET" && setTimeout(function () {
+    createXHR(settings);
+  }, settings.frequency || 30000);
+}
+
+/**
+ * Define what will be the job of a poller we create.
+ * After creating a poller, we can interface with it as if it was
+ * a WebSocket.
+ *
+ * Sending data to it will initiate a PUT/POST/DELETE after which time
+ * it will go back to polling with GET
+ */
+pltask = 'function () {'
+
+       + '  var globals = {{GLOBALS}},'
+       + '      process = {{PROCESS}};'
+
+            /*
+             * Make sure ajax workers can use our stringify function.
+             */
+       +    stringify.toString()
+
+            /*
+             * Make sure ajax workers can use our xhr function.
+             */
+       +    createXHR.toString()
+
+            /*
+             * Use our global config to start polling.
+             */
+       + '  createXHR(globals);'
+
+            /*
+             * When the user sends us a message, make an ajax request.
+             * `msg` should be a settings object.
+             */
+       + '  return function (msg) {'
+       + '    msg.url = globals.url;'
+       + '    !msg.timeout && (msg.timeout = globals.timeout);'
+       + '    !msg.headers && (msg.headers = globals.headers);'
+       + '    createXHR(msg);'
+       + '  };'
+       + '}'
+       ;
 
 /**
  * Define what will be the job of the worker we create.
@@ -13,49 +301,50 @@ var global    = window,
  * invoked to process the result. The processed result will then
  * be sent back to our main thread. 
  */
-task  = 'function () {'
+wstask = 'function () {'
 
-           /*
-            * Create a new WebSocket and track its original send method.
-            */
-      + '  var socket   = new WebSocket({{SOCKETURL}}),'
-      + '      origSend = WebSocket.prototype.send,'
-      + '      callback = {{CALLBACK}};'
-      
-           /*
-            * Overwrite the send method to compensate for the potential race
-            * conditions where sockets have to be asynchronously opened
-            * before messages can be sent.
-            */
-      + '  WebSocket.prototype.send = function (msg) {'
-      + '    if (this.readyState !== 1) {'
-      + '      return setTimeout(function () {this.send(msg)}.bind(this), 10);'
-      + '    }'
-      + '    return origSend.call(this, msg);'
-      + '  };'
+            /*
+             * Create a new WebSocket and track its original send method.
+             */
+       + '  var socket   = new WebSocket({{SOCKETURL}}),'
+       + '      origSend = WebSocket.prototype.send,'
+       + '      callback = {{CALLBACK}};'
+       
+            /*
+             * Overwrite the send method to compensate for the potential race
+             * conditions where sockets have to be asynchronously opened
+             * before messages can be sent.
+             */
+       + '  WebSocket.prototype.send = function (msg) {'
+       + '    if (this.readyState !== 1) {'
+       + '      return setTimeout(function () {this.send(msg)}.bind(this), 10);'
+       + '    }'
+       + '    return origSend.call(this, msg);'
+       + '  };'
 
-           /*
-            * When we get a message from the socket, process it and
-            * pass it back to the user.
-            */
-      + '  socket.onmessage = function (msg) {'
-      + '    self.postMessage(callback ? callback(msg.data) : msg.data);'
-      + '  };'
+            /*
+             * When we get a message from the socket, process it and
+             * pass it back to the user.
+             */
+       + '  socket.onmessage = function (msg) {'
+       + '    self.postMessage(callback ? callback(msg.data) : msg.data);'
+       + '  };'
       
-           /*
-            * When the user sends us a message, pass it along to the socket.
-            */
-      + '  return function (msg) {'
-      + '    socket.send(msg);'
-      + '  };'
-      + '}'
-      ;
+            /*
+             * When the user sends us a message, pass it along to the socket.
+             */
+       + '  return function (msg) {'
+       + '    socket.send(msg);'
+       + '  };'
+       + '}'
+       ;
+
 
 /**
  * @constructor
  *
  * Generates an object for interfacing with a WebSocket through
- * a web worker.
+ * a web worker: a Solid Fuel Socket Booster.
  *
  * @param {String}   url - The websocket url.
  * @param {Function} cb  - Optional. A callback for how the worker should
@@ -70,8 +359,8 @@ task  = 'function () {'
  */
 function SFSB(url, cb) {
   var worker       = secretary(),
-      specificTask = task.replace('{{SOCKETURL}}', '"' + url + '"')
-                         .replace('{{CALLBACK}}', cb ? cb.toString() : 'null');
+      specificTask = wstask.replace('{{SOCKETURL}}', '"' + url + '"')
+                           .replace('{{CALLBACK}}', cb?cb.toString():'null');
   
   /*
    * Give the worker its job.
@@ -95,30 +384,155 @@ SFSB.prototype = {
    * @param msg - The message to send.
    */
   "send" : function (msg) {
-    return this.worker.postMessage(msg);
+    this.worker.postMessage(msg);
+    return this;
   },
 
   /**
    * Close the worker and by extension the socket.
    */
   "close" : function () {
-    return this.worker.close();
+    this.worker.close();
+    return this;
   },
 
   /**
    * Listen for events on the socket in the worker.
    */
   "addEventListener" : function (evt, fn) {
-    return this.worker.addListener(evt, fn);
+    this.worker.addListener(evt, fn);
+    return this;
+  }
+};
+
+/**
+ * @constructor
+ *
+ * Creates an object for polling a server inside a Web Worker:
+ * a Solid Fuel Ajax Poller.
+ *
+ * @param {Object} settings - A settings object for ajax.
+ *
+ * @key {String}   url       - The request URL.
+ * @key {Object}   data      - Data to send with GET requests.
+ * @key {Number}   timeout   - How long before the request gives up. Defaults to 10000.
+ * @key {Number}   frequency - How often to poll the server. Defaults to 30000.
+ * @key {Function} process   - A function for processing successful data.
+ * @key {Object}   headers   - Defaults to native defaults. Should
+ *                             be in the format of {contentType: 'text/plain'}
+ *
+ * @returns {undefined}
+ */
+function SFAP(settings) {
+  var worker, specificTask;
+
+  /*
+   * Don't let users poll with posts, etc.
+   */
+  settings.type = 'GET';
+
+  /*
+   * Create a worker and its associated task.
+   */
+  worker       = secretary();
+  specificTask = pltask.replace('{{GLOBALS}}', JSON.stringify(settings))
+                       .replace('{{PROCESS}}', settings.process ? settings.process.toString() : 'null');
+
+  /*
+   * Give the worker its job.
+   */
+  worker.postJob(specificTask, true);
+
+  /*
+   * Add a message event listener. When we get a message back from the worker,
+   * invoke all callbacks for that kind of message.
+   */
+  worker.addListener('message', function (msg) {
+    this['on' + msg.data.result].forEach(function (callback) {
+      callback(msg.data);
+    });
+    this.onmessage.forEach(function (callback) { callback(msg.data) });
+  }.bind(this));
+
+  /*
+   * Add an error event listener. When the worker reports an error, invoke
+   * error callbacks.
+   */
+  worker.addListener('error', function (msg) {
+    var toPass = {"result" : "error", "response" : msg, "status" : -1};
+    this.onerror.forEach(function (callback) { callback(toPass) });
+    this.onmessage.forEach(function (callback) { callback(toPass) });
+  });
+
+  /*
+   * Store the worker and other important info.
+   */
+  this.worker    = worker;
+  this.settings  = settings;
+  this.onsuccess = [];
+  this.onerror   = [];
+  this.onmessage = [];
+}
+
+/**
+ * The methods of the SFAP object.
+ */
+SFAP.prototype = {
+
+  /**
+   * Send a POST request to the poller. Does not interrupt polling.
+   * Can not use a different URL than the URL being polled. All other
+   * settings may be overwritten.
+   */
+  "post" : function (settings) {
+    msg.type = 'POST';
+    this.worker.postMessage(msg);
+    return this;
+  },
+
+  /**
+   * Send a PUT request to the poller. Does not interrupt polling.
+   * Can not use a different URL than the URL being polled. All other
+   * settings may be overwritten.
+   */
+  "put" : function (settings) {
+    msg.type = 'PUT';
+    this.worker.postMessage(msg);
+    return this;
+  },
+
+  /**
+   * Send a DELETE request to the poller. Does not interrupt polling.
+   * Can not use a different URL than the URL being polled. All other
+   * settings may be overwritten.
+   */
+  "del" : function (settings) {
+    msg.type = 'DELETE';
+    this.worker.postMessage(msg);
+    return this;
+  },
+
+  /**
+   * Allow users to listen for success, error, and message events
+   * so that they can run callbacks in those cases. The "message"
+   * event essentially equates to an "always" callback.
+   */
+  "addEventListener" : function (evt, callback) {
+    this['on' + evt.toLowerCase()].push(callback);
+    return this;
   }
 };
 
 /**
  * Assign this sucker to the global scope.
  */
-global.SFSB = SFSB;
+global.SF = {
+  "socketBooster" : SFSB,
+  "ajaxPoller"    : SFAP
+};
 
-},{"./secretary":2}],2:[function(require,module,exports){
+}).call(this,require("/Users/john/Sites/github/sfsb/node_modules/gulp-browserify/node_modules/browserify/node_modules/insert-module-globals/node_modules/process/browser.js"))
+},{"./secretary":3,"/Users/john/Sites/github/sfsb/node_modules/gulp-browserify/node_modules/browserify/node_modules/insert-module-globals/node_modules/process/browser.js":1}],3:[function(require,module,exports){
 
 /**
  * Creates a special genius web worker.
@@ -333,4 +747,4 @@ function createWorker() {
  * Export module code.
  */
 module.exports = createWorker;
-},{}]},{},[1])
+},{}]},{},[2])
